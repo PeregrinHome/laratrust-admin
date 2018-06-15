fdb307f115cb347931e0df7e583a5e405741e7cc
cvaize <cvaize@gmail.com>
2018-06-15 23:02:36 +0500

    Initial commit

---
+++ src/LaratrustAdminServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace LaratrustAdmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModulesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+
+
+        //регистрируем конфиг
+//        $this->mergeConfigFrom(__DIR__ . '/config/module.php', 'module');
+//        $this->publishes([
+//            __DIR__.'/config' => base_path('config'),
+//        ]);
+//        $modules = config("module.modules");
+//        $dir = app_path().'/Modules';
+//        // dd($dir);
+//        if($modules) {
+//            while (list(,$module) = each($modules)){
+//                // dd(file_exists($dir.'/'.$module.'/Routes/routes.php'));
+//                //Подключаем роуты для модуля
+//                if(file_exists($dir.'/'.$module.'/Routes/routes.php')) {
+//                    $this->loadRoutesFrom($dir.'/'.$module.'/Routes/routes.php');
+//                }
+//                //Загружаем View
+//                //view('test::admin')
+//                if(is_dir($dir.'/'.$module.'/Views')) {
+//                    $this->loadViewsFrom($dir.'/'.$module.'/Views', $module);
+//                }
+//                //Подгружаем миграции
+//                if(is_dir($dir.'/'.$module.'/Migration')) {
+//                    $this->loadMigrationsFrom($dir.'/'.$module.'/Migration');
+//                }
+//                //Подгружаем переводы
+//                //trans('test::messages.welcome')
+//                if(is_dir($dir.'/'.$module.'/Lang')) {
+//                    $this->loadTranslationsFrom($dir.'/'.$module.'/Lang', $module);
+//                }
+//            }
+//        }
+    }
+
+    public function register()
+    {
+    }
+}
