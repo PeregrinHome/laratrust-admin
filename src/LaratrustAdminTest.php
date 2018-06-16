fc435382b186820ce6edbd4f340997db26b36140
cvaize <cvaize@gmail.com>
2018-06-16 12:52:45 +0500

    Test

---
+++ src/LaratrustAdminTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cvaize
+ * Date: 16.06.2018
+ * Time: 12:51
+ */
+
+namespace LaratrustAdmin;
+
+
+class LaratrustAdminTest
+{
+    public function test(){
+        var_dump('Test');
+    }
+}
